371106007e7f26f5aa95afa646c667a2c79d24f0
AltaraceO <orialtarace@gmail.com>
2022-06-02 14:26:46 +0300

    feat(service): add a user service
    a new user service will keep track if a user is logged in and enable viewing if they are current  get their user information and log them out in the future

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+interface User {
+  email: string;
+  password: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+
+//this is exported to ensure that a user is updated globally and can be access from anywhere in the app
+export class UserService {
+  user$ = new BehaviorSubject<User | null>(null);
+
+  constructor() {}
+
+  //this enables seeing if there is a current user of if it is null
+  currentUser(): Observable<User | null> {
+    return this.user$;
+  }
+
+  //this returns the user and its value
+  valueUser(): User | null {
+    return this.user$.value;
+  }
+
+  // this allows to update the user if needed
+  updateUser(user: User): void {
+    this.user$.next(user);
+  }
+}
--- src/app/views/sign-in/sign-in.page.ts
+++ src/app/views/sign-in/sign-in.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
+
+interface User {
+  email: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-sign-in',
@@ -9,7 +15,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class SignInPage implements OnInit {
   formItems: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private user: UserService) {
+    // this.current = user.updateUser(this.formItems.value);
+  }
 
   ngOnInit() {
     //this builds a form with two items (email,password) and runs the validations on it - call connected in HTML
@@ -19,7 +27,15 @@ export class SignInPage implements OnInit {
     });
   }
 
+  updateCurrent(): void {
+    this.user.updateUser(this.formItems.value);
+  }
+
   userLogin() {
-    console.log('loggedin');
+    console.log('loggedIn');
+    console.log(this.formItems);
+
+    this.updateCurrent();
+    console.log(this.user.valueUser());
   }
 }
