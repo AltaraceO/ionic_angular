e3e67bf1f34e87c528a25e511d5bbd17ca384617
AltaraceO <orialtarace@gmail.com>
2022-06-02 18:07:38 +0300

    feat(elements): user name on chat
    add the current users name in the chat window

--- src/app/views/chat/chat.page.ts
+++ src/app/views/chat/chat.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonContent } from '@ionic/angular';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -7,14 +8,11 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./chat.page.scss'],
 })
 export class ChatPage implements OnInit {
+  //*get the current view so that messages move to the top
+  @ViewChild(IonContent) content: IonContent;
   messages = [
     {
       user: 'bot',
-      time: 1654176594,
-      text: 'Welcome to our chat, what would you like to share?',
-    },
-    {
-      user: 'bit',
       time: 1654176594,
       text: 'Welcome to our chat, what would you like to share?',
     },
@@ -25,13 +23,15 @@ export class ChatPage implements OnInit {
   constructor(private user: UserService) {}
 
   ngOnInit() {
-    // this.currentUser = this.user.valueUser()?.email;
-    // console.log(this.currentUser);
-    this.currentUser = 'bit';
+    this.currentUser = this.user.valueUser()?.email;
+    console.log(this.currentUser);
+    // this.currentUser = 'bit';
   }
 
   sendMessage() {
-    if (this.newMessage === undefined) {
+    console.log(this.currentUser);
+
+    if (this.newMessage === undefined || this.newMessage === '') {
       return;
     }
 
@@ -39,6 +39,13 @@ export class ChatPage implements OnInit {
       user: this.currentUser,
       time: new Date().getTime(),
       text: this.newMessage,
+    });
+
+    this.newMessage = '';
+    //*view will move to the bottom so that old messages disappear to the top
+    // * the time  out is set so that it happens after the view is sized after the new message is added
+    setTimeout(() => {
+      this.content.scrollToBottom();
     });
   }
 }
