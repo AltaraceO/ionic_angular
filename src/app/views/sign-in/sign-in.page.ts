38b017bdaa933e160b2fdbe7a7012474998412a9
AltaraceO <orialtarace@gmail.com>
2022-06-02 15:08:42 +0300

    fix(constructor): private to public
    enable the use of user in the ts as well as HTML files

--- src/app/views/sign-in/sign-in.page.ts
+++ src/app/views/sign-in/sign-in.page.ts
@@ -10,7 +10,7 @@ import { UserService } from 'src/app/services/user.service';
 export class SignInPage implements OnInit {
   formItems: FormGroup;
 
-  constructor(private fb: FormBuilder, private user: UserService) {}
+  constructor(private fb: FormBuilder, public user: UserService) {}
 
   ngOnInit() {
     //this builds a form with two items (email,password) and runs the validations on it - call connected in HTML
